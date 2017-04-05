255d650ecc3f8f3e2010393ff926940d1e9550b4
Nirat Attri <nirat.a@practo.com>
2017-04-05 13:53:17 +0530

    Added new routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var authors = require('./routes/authors');
+var books = require('./routes/books');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/authors', authors);
+app.use('/books', books);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
